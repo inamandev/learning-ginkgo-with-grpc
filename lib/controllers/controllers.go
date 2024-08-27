3904311c697b80c46b807e75df70daa59adb1b7f
Naman Kathuria <namankathuria@hotmail.com>
2024-08-27 05:55:50 -0600

    first draft for get student api

--- lib/controllers/controllers.go
+++ lib/controllers/controllers.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"log"
 
+	"github.com/inamandev/learning-ginkgo-with-grpc/lib/store"
 	pb "github.com/inamandev/learning-ginkgo-with-grpc/students_proto"
 )
 
@@ -12,8 +14,33 @@ type Server struct {
 }
 
 func (*Server) Get(ctx context.Context, req *pb.StudentId) (*pb.Response, error) {
-	log.Println(req.Id)
-	return nil, nil
+	// validate input
+	// case 1 if id is zero or below
+	if req.Id <= 0 {
+		err := errors.New("invalid student id. Kindly provide valid id")
+		log.Println(err.Error())
+		return &pb.Response{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		}, err
+	}
+	student := &pb.StudentPayload{
+		Id: req.Id,
+	}
+	err := store.DB.GetStudent(student)
+	if err != nil {
+		return &pb.Response{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		}, err
+
+	}
+	return &pb.Response{
+		Success: true,
+		Data:    student,
+	}, nil
 }
 func (*Server) Create(ctx context.Context, req *pb.StudentPayload) (*pb.Response, error) {
 	return nil, nil
--- lib/store/store.go
+++ lib/store/store.go
@@ -2,7 +2,9 @@ package store
 
 import (
 	"database/sql"
+	"log"
 
+	pb "github.com/inamandev/learning-ginkgo-with-grpc/students_proto"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -21,4 +23,20 @@ func Connect() error {
 	}
 	DB = &Pool{db}
 	return DB.Ping()
+}
+
+func (db Pool) GetStudent(payload *pb.StudentPayload) error {
+	stmt, err := db.Prepare(`SELECT name, age, email FROM students WHERE id = ?`)
+	if err != nil {
+		log.Println("unable to prepare statement: ", err)
+		return err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(payload.Id).Scan(&payload.Name, &payload.Age, &payload.Email)
+	if err != nil {
+		log.Println("unable to execute statement: ", err)
+		return err
+	}
+	return nil
 }
